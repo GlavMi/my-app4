ad7b498700cc6a4a7884f11e0cfccaf10a3a553a
GlavMi <tepan5310gmail.com>
2022-06-10 23:57:35 +0500

    commit 4

---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Nav = () => {
+    return  <div className={'nav'}>
+
+        <div>
+            <a> Profile </a>
+        </div>
+        <div>
+            <a> Messages </a>
+        </div>
+        <div>
+            <a> News </a>
+        </div>
+        <div>
+            <a> Setting </a>
+        </div>
+
+    </div>
+}
+export default  Nav
