f99a8aafcd42e1b96217d436ab741eebe00b555d
GlavMi <tepan5310gmail.com>
2022-06-10 23:56:24 +0500

    commit 3

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = () => {
+    return <div className={'header'}>
+        <img src = 'https://avatars.mds.yandex.net/i?id=c4d98252587f28d69315188e3f2db7d2-5651184-images-thumbs&n=13'/>
+    </div>
+}
+export default Header
