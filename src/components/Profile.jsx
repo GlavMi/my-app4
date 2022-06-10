338b1a48ef9539af28f8fc6c3ec63c783500f9d6
GlavMi <tepan5310gmail.com>
2022-06-10 23:58:36 +0500

    commit 5

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className={'content'}>
+        <div>
+            <img src = 'https://n1s1.elle.ru/67/87/f5/6787f53ff378de2665b5aeeb9f0678c4/2338x1256_0xd42ee42a_17048057571391938245.jpeg'/>
+        </div>
+
+        <div>
+            Ava + discription
+        </div>
+
+        <div>
+            My posts
+        </div>
+
+        <div>
+            New posts
+        </div>
+
+        <div>
+
+            <div> Post 1 </div>
+
+            <div> Post 2 </div>
+
+        </div>
+    </div>
+}
+export default Profile
