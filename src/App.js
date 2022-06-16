0b03359a6b066677a0516c6b7d89760574986997
GlavMi <tepan5310gmail.com>
2022-06-16 15:33:48 +0500

    commit-18

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
-const App= () => {
-  return (
-      <div className={'app-wrapper'}>
+const App = () => {
+    return (
+        <div className={'app-wrapper'}>
 
-        <Header/>
-        <Nav/>
-        <Profile/>
+            <Header/>
 
-      </div>
-  )
+            <Nav/>
+
+            <div className={'app-wrapper-content'}>
+            <Dialogs/>
+            </div>
+
+        {/*
+       <div className={'app-wrapper-content'}>
+       <Profile/>
+       </div>
+       */}
+
+        </div>
+    )
 
 }
 
