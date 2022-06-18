5039dfe11000dfb1ce1f81690cd0730c8f513b6b
GlavMi <tepan5310gmail.com>
2022-06-18 15:06:02 +0500

    commit-20a

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,23 +2,25 @@ import React from "react";
 import classes from "./Nav.module.css";
 import {NavLink} from "react-router-dom";
 
-const Nav = () => {
+
+
+const Nav = (props) => {
     return  <div className={classes.nav} >
 
         <div className={classes.item}>
-            <NavLink to = '/profile'  >  Profile </NavLink>
+            <NavLink to = '/profile' className={({isActive}) => (isActive ? classes.active:classes.item)} >  Profile </NavLink>
         </div>
         <div className = {classes.item} >
-            <NavLink to = '/dialogs' activeClassName={classes.active}>  Messages </NavLink>
+            <NavLink to = '/dialogs' className={({isActive}) => (isActive ? classes.active:classes.item)}>  Messages </NavLink>
         </div>
         <div className={classes.item} >
-            <NavLink to = '/news'>  News </NavLink>
+            <NavLink to = '/news' className={({isActive}) => (isActive ? classes.active:classes.item)}>  News </NavLink>
         </div>
         <div className={classes.item}>
-            <NavLink to = '/music'> Music </NavLink>
+            <NavLink to = '/music' className={({isActive}) => (isActive ? classes.active:classes.item)}> Music </NavLink>
         </div>
         <div className={classes.item}>
-            <NavLink to = '/setting'> Setting </NavLink>
+            <NavLink to = '/setting' className={({isActive}) => (isActive ? classes.active:classes.item)}> Setting </NavLink>
         </div>
 
     </div>
